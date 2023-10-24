814872b2ba7d2dc5f72e2fa530ea65a0ff8d8854
afzal <afzalnaved0000@gmail.com>
2023-10-24 22:54:23 +0530

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
+import { useState  , useEffect} from 'react'
 import './App.css'
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
+import { login , logout } from './store/authSlice';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch=useDispatch()
 
-  return (
-    <>
-    HEllO
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser().then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
